fe3b92f48a0aa7a30242eb1e5e23d3f13ff7d49c
linyize <linyize@gmail.com>
2014-12-17 15:38:56 +0800

    原生代码调用js的统一约定

--- html.bundle/webridge.js
+++ html.bundle/webridge.js
@@ -1,16 +1,30 @@
 
+function wbNativeToHTML(jsCommand, jsParams) {
+    var jsParamsString = JSON.stringify(jsParams);
+    var jsScript = jsCommand + '(' + jsParamsString + ')';
+    var result = eval(jsScript);
+    var isAndroid = navigator.userAgent.match('Android');
+    if (isAndroid) {
+        window.androidWebridge.postMessage(JSON.stringify(result));
+    }
+    else {
+        // iOS or other platform
+        return result;
+    }
+}
+
 function wbHTMLToNative(command, params, callback) {
-    var message={"command":command,"params":params,"callback":callback};
-    var isiOS=navigator.userAgent.match('iPad')
+    var message = {"command":command,"params":params,"callback":callback};
+    var isiOS = navigator.userAgent.match('iPad')
     || navigator.userAgent.match('iPhone')
     || navigator.userAgent.match('iPod');
-    var isAndroid=navigator.userAgent.match('Android');
-    var isDesktop=(!isiOS && !isAndroid);
+    var isAndroid = navigator.userAgent.match('Android');
+    var isDesktop = (!isiOS && !isAndroid);
     if (isiOS) {
         window.webkit.messageHandlers.webridge.postMessage({body:message});
     }
     else if (isAndroid) {
-        // TODO: android
+        window.androidWebridge.postMessage(JSON.stringify(message));
     }
     else if (isDesktop) {
         // TODO: desktop
